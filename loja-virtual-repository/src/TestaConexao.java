7114e500a8636e51c05f45a63f6fc6377f6adee8
Windows 10 <Windows 10@192.168.0.7>
2022-11-11 21:20:32 -0300

    Update

--- loja-virtual-repository/src/TestaConexao.java
+++ loja-virtual-repository/src/TestaConexao.java
@@ -5,12 +5,12 @@ import java.sql.SQLException;
 public class TestaConexao {
 
 	public static void main(String[] args) throws SQLException {
-		
+
 		System.out.println("Abrindo conexao...");
 
 		Connection connection = DriverManager.getConnection(
 				"jdbc:mysql://localhost/loja_virtual?useTimezone=true&serverTimezone=UTC", "root", "root");
-		
+
 		System.out.println("Fechando conexao...");
 
 		connection.close();
