78ae7b552bd80f0ebc419888d23e7f8868bfe7e1
Windows 10 <Windows 10@DESKTOP-ANGJ5V6>
2022-11-21 16:25:47 -0300

    Update

--- loja-virtual-repository/src/TestaInsercaoComParametro.java
+++ loja-virtual-repository/src/TestaInsercaoComParametro.java
@@ -4,17 +4,18 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 public class TestaInsercaoComParametro {
-	
+
 	public static void main(String[] args) throws SQLException {
-		
-		String nome = "Mouse";
+
+		String nome = "Mouse'";
 		String descricao = "Mouse Sem Fio";
 		ConnectionFactory factory = new ConnectionFactory();
 		Connection connection = factory.recuperaConexao();
 
+		String sql = "INSERT INTO PRODUTO (nome, descricacao) VALUES ('" + nome + "', '" + descricao + "')";
+		System.out.println(sql);
 		Statement stm = connection.createStatement();
-		stm.execute("INSERT INTO PRODUTO (nome, descricacao) VALUES ('" + nome + "', '" + descricao + "')"
-				, Statement.RETURN_GENERATED_KEYS);
+		stm.execute(sql, Statement.RETURN_GENERATED_KEYS);
 
 		ResultSet rst = stm.getGeneratedKeys();
 		while (rst.next()) {
