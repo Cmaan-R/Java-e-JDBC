5cdb816ce63a14a87a2ed71136bad398ab489b8e
Windows 10 <Windows 10@192.168.200.171>
2022-11-22 15:06:52 -0300

    Update

---
+++ loja-virtual-repository/src/TestaInsercaoComProduto.java
@@ -0,0 +1,26 @@
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import br.com.alura.jdbc.ConnectionFactory;
+import br.com.alura.jdbc.modelo.Produto;
+
+public class TestaInsercaoComProduto {
+
+	public static void main(String[] args) throws SQLException {
+
+		Produto comoda = new Produto("Cômoda", "Cômoda Vertical");
+
+		try (Connection connection = new ConnectionFactory().recuperaConexao()) {
+
+			String sql = "INSERT INTO PRODUTO (NOME, DESCRICACAO) VALUES (? , ?)";
+
+			try (PreparedStatement pstm = connection.prepareStatement(sql, java.sql.Statement.RETURN_GENERATED_KEYS)) {
+				
+				pstm.setString(1, comoda.getNome());
+				pstm.setString(2, comoda.getDescricao());
+			}
+
+		}
+	}
+}
