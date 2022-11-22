d70d93e9bd7793540e32c99b48226362211b8e56
Windows 10 <Windows 10@192.168.200.171>
2022-11-22 15:20:25 -0300

    Update

---
+++ loja-virtual-repository/src/br/com/alura/jdbc/TestaInsercaoComProduto.java
@@ -0,0 +1,21 @@
+package br.com.alura.jdbc;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import br.com.alura.jdbc.modelo.Produto;
+
+public class TestaInsercaoComProduto {
+
+	public static void main(String[] args) throws SQLException {
+
+		Produto comoda = new Produto("Cômoda", "Cômoda Vertical");
+
+		try (Connection connection = new ConnectionFactory().recuperaConexao()) {
+
+			new PersistenciaProduto(connection).salvarProduto(comoda);
+			
+		}
+		System.out.println(comoda);
+	}
+}
