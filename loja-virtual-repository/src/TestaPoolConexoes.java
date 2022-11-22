b5bc9db723aec73ae9b8af56fb07c6f540f989f4
Windows 10 <Windows 10@192.168.200.171>
2022-11-22 14:52:49 -0300

    Update

--- loja-virtual-repository/src/TestaPoolConexoes.java
+++ loja-virtual-repository/src/TestaPoolConexoes.java
@@ -1,16 +1,15 @@
 import java.sql.SQLException;
 
 public class TestaPoolConexoes {
-	
+
 	public static void main(String[] args) throws SQLException {
-		
+
 		ConnectionFactory connectionFactory = new ConnectionFactory();
-		
-		for (int i = 0; i < 20 ; i++) {
+
+		for (int i = 0; i < 20; i++) {
 			connectionFactory.recuperaConexao();
 			System.out.println("Conexão de número: " + i);
 		}
-		
 		
 	}
 
