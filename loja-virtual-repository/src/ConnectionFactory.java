8ee03ac1c5388fabe90d9d4e3737d0f190462b08
Windows 10 <Windows 10@192.168.200.171>
2022-11-22 11:57:51 -0300

    Update

--- loja-virtual-repository/src/ConnectionFactory.java
+++ loja-virtual-repository/src/ConnectionFactory.java
@@ -1,5 +1,4 @@
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
 
 import javax.sql.DataSource;
@@ -7,8 +6,8 @@ import javax.sql.DataSource;
 import com.mchange.v2.c3p0.ComboPooledDataSource;
 
 public class ConnectionFactory {
-	
-	public DataSource dataSource; 
+
+	public DataSource dataSource;
 
 	public ConnectionFactory() {
 		ComboPooledDataSource comboPooledDataSource = new ComboPooledDataSource();
